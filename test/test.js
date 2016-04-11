21511de34d1eb3e635877aa6bf2d5ed64a96d190
swoskowiak <swoskowiak@gmail.com>
2016-04-11 14:48:38 -0400

    getting travis ci working

--- test/test.js
+++ test/test.js
@@ -5,24 +5,33 @@ var path = require('path'),
   assert = require('yeoman-assert');
 
 describe('Fresh Generator', function () {
-  before(function (done) {
-    helpers.run(path.join(__dirname, '../app'))
-      .withPrompts({name: 'testName', description: 'testDesc', mocha: 'y', coveralls: 'y'})
-      .on('end', function () {
-        done();
+  beforeEach(function () {
+    this.tester = helpers.run(path.join(__dirname, '../app'))
+      .withPrompts({
+        name: 'testName',
+        description: 'testDesc',
+        mocha: 'y',
+        coveralls: 'y'
       });
   });
+  describe('generates all files', function () {
+    beforeEach(function (done) {
+      this.tester.on('end', done);
+    });
 
-  it ('generates all the appropriate files given "y" to all answers', function () {
-    // Check everything was made
-    assert.file([
-      '.gitignore',
-      '.jshintrc',
-      '.jscsrc',
-      '.travis.yml',
-      'package.json',
-      'index.js',
-      'test/test.js'
-    ]);
+    it ('generates all the appropriate files given "y" to all answers', function () {
+      // Check everything was made
+      assert.file([
+        '.gitignore',
+        '.jshintrc',
+        '.jscsrc',
+        '.travis.yml',
+        'package.json',
+        'index.js',
+        'test/test.js'
+      ]);
+    });
+
   });
+
 });
