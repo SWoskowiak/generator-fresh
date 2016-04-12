8a29fa58014f0ad2e79269b936e9b8c78566dd9c
swoskowiak <swoskowiak@gmail.com>
2016-04-11 20:33:05 -0400

    tests

--- test/test.js
+++ test/test.js
@@ -5,33 +5,27 @@ var path = require('path'),
   assert = require('yeoman-assert');
 
 describe('generator-fresh:app', function () {
-  beforeEach(function () {
-    this.tester = helpers.run(path.join(__dirname, '../app'))
+  before(function (done) {
+    helpers.run(path.join(__dirname, '../app'))
       .withPrompts({
         name: 'testName',
         description: 'testDesc',
         mocha: 'y',
         coveralls: 'y'
-      });
-  });
-  describe('generates all files', function () {
-    beforeEach(function (done) {
-      this.tester.on('end', done);
-    });
-    console.log(path.join(__dirname, '../app'));
-    it ('generates all the appropriate files given "y" to all answers', function () {
-      // Check all files exist where they should
-      assert.file([
-        '.gitignore',
-        '.jshintrc',
-        '.jscsrc',
-        '.travis.yml',
-        'package.json',
-        'index.js',
-        'test/test.js'
-      ]);
-    });
-
+      })
+      .on('end', done);
   });
 
+  it ('generates all the appropriate files given "y" to all answers', function () {
+    // Check all files exist where they should
+    assert.file([
+      '.gitignore',
+      '.jshintrc',
+      '.jscsrc',
+      '.travis.yml',
+      'package.json',
+      'index.js',
+      'test/test.js'
+    ]);
+  });
 });
