48bffbac819a71479c5d117e9ebced2c5c8cfd2b
swoskowiak <swoskowiak@gmail.com>
2016-04-12 11:28:57 -0400

    revert back to yeoman assert

--- test/test.js
+++ test/test.js
@@ -2,22 +2,11 @@
 
 var path = require('path'),
   helpers = require('yeoman-test'),
-  chai = require('chai'),
-  expect = chai.expect;
-
-
-chai.use(require('chai-fs'));
-
+  assert = require('yeoman-assert');
 
 describe('generator-fresh:app', function () {
-  var tempDir = '';
-
   before(function (done) {
     helpers.run(path.join(__dirname, '../generators/app'))
-      .inTmpDir(function (dir) {
-        tempDir = dir;
-        console.log('DIR:', dir, '\n\n');
-      })
       .withPrompts({
         name: 'testName',
         description: 'testDesc',
@@ -27,24 +16,20 @@ describe('generator-fresh:app', function () {
       .on('end', done);
   });
 
-  it ('generates all the appropriate files given "y" to all answers', function () {
-    // Check all files exist where they should
-    // assert.file([
-    //   '.gitignore',
-    //   '.jshintrc',
-    //   '.jscsrc',
-    //   '.travis.yml',
-    //   'package.json',
-    //   'index.js',
-    //   'test/test.js'
-    // ]);
-    expect(tempDir + '/.gitignore').to.be.a.file();
-    //expect(tempDir + '/.jscsrc').to.be.a.file();
-    expect(tempDir + '/.jshintrc').to.be.a.file();
-    expect(tempDir + '/.travis.yml').to.be.a.file();
-    expect(tempDir + '/index.js').to.be.a.file();
-    expect(tempDir + '/test/test.js').to.be.a.file();
-    expect(tempDir + '/package.json').to.be.a.file();
+  describe('generates all files', function () {
 
+    it ('generates all the appropriate files given "y" to all answers', function () {
+      // Check all files exist where they should
+      assert.file([
+        '.gitignore',
+        '.jshintrc',
+        '.jscsrc',
+        '.travis.yml',
+        'package.json',
+        'index.js',
+        'test/test.js'
+      ]);
+    });
   });
+
 });
